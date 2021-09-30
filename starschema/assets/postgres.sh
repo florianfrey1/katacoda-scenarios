a4c6d8da73251f05e2c1d163b115c1ebf29466d0
Florian Frey <florianfrey@live.de>
2021-09-30 11:07:25 +0200

    network added

--- starschema/assets/postgres.sh
+++ starschema/assets/postgres.sh
@@ -1,3 +1,3 @@
 docker-compose up -d
 # while ! docker-compose exec rabbitmq /is_ready.sh; sleep 1; done
-docker run --link postgres:postgres --rm dokku/wait -p 5432
+docker run --link postgres_database:postgres_database --net bridge --rm dokku/wait -p 5432
