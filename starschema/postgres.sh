a4071fbb2f9c299bfa039acae5a8ff80795b4062
Florian Frey <florianfrey@live.de>
2021-09-30 13:16:57 +0200

    changes

--- starschema/postgres.sh
+++ starschema/postgres.sh
@@ -1,5 +1,5 @@
 # Run the postgres database via the docker-compose command
-docker-compose up -d
+docker-compose up
 
 # Wait for the postgres container to boot up
 ./wait-for-it.sh -t 0 127.0.0.1:5432 -- echo "postgres database is ready"
