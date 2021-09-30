00bb141f6530727481740723c9f29a1e136f18ca
Florian Frey <florianfrey@live.de>
2021-09-30 13:44:27 +0200

    output enhancement

--- starschema/init.sh
+++ starschema/init.sh
@@ -15,6 +15,8 @@ npm i pg
 
 # Clear console and print ready message.
 clear
-echo "Die Umgebung ist jetzt bereit!"
-echo " - Die Postgres-Datenbank ist bereit"
-echo " - Das NodeJS-Projekt ist angelegt"
+{
+    echo "Die Umgebung ist jetzt bereit!"
+    echo " - Die Postgres-Datenbank ist bereit"
+    echo " - Das NodeJS-Projekt ist angelegt"
+} &> /dev/null
