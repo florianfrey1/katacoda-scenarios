d391be6ca3fb059aff823b20f84584488bec92fc
Florian Frey <florianfrey@live.de>
2021-09-30 12:59:37 +0200

    add wait script

--- starschema/postgres.sh
+++ starschema/postgres.sh
@@ -1,11 +1,8 @@
 # Run the postgres database via the docker-compose command
 docker-compose up -d
 
-# ./wait-for-it.sh -t 0 127.0.0.1:5432 -- echo "postgres database is ready"
-
-# Wait for the postgres container to start up using the small
-# dokku/wait container: https://github.com/dokku/docker-wait.
-# docker run --net bridge --rm dokku/wait -p 5432
+# Wait for the postgres container to boot up
+./wait-for-it.sh -t 0 127.0.0.1:5432 -- echo "postgres database is ready"
 
 # Create databases.
 docker exec -it postgres_database bash
