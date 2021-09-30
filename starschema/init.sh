d65a7b1270cc2c4207797d72c1e3e3a705458f8b
Florian Frey <florianfrey@live.de>
2021-09-30 14:06:56 +0200

    changes

--- starschema/init.sh
+++ starschema/init.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Run the postgres database via the docker-compose command
-docker-compose up -d > /dev/null
+docker-compose up -d &> /dev/null
 
 # Wait for the postgres container to boot up
-./wait-for-it.sh -t 0 127.0.0.1:5432 > /dev/null
+./wait-for-it.sh -t 0 127.0.0.1:5432 &> /dev/null
 
 # Initialize the direktory for the node.js projekt (in the
 # current /root folder).
-npm init -y > /dev/null
+npm init -y &> /dev/null
 
 # Install the pg module via npm. The pg module is used to
 # connect from node js to the postgres databse:
 # https://github.com/brianc/node-postgres.
-npm i pg > /dev/null
+npm i pg &> /dev/null
 
 # Clear console and print ready message.
 ./ready.sh
