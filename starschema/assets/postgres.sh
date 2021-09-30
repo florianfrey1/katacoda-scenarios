69a7bd5d02c2f7061d052d5776eec64b12d8e006
Florian Frey <florianfrey@live.de>
2021-09-30 11:32:28 +0200

    order changed

--- starschema/assets/postgres.sh
+++ starschema/assets/postgres.sh
@@ -1,13 +1,13 @@
+wget https://github.com/ufoscout/docker-compose-wait/releases/download/2.9.0/wait
+chmod +x wait
+./wait
+
 # Run the postgres database via the docker-compose command
 docker-compose up -d
 
 # Wait for the postgres container to start up using the small
 # dokku/wait container: https://github.com/dokku/docker-wait.
 # docker run --net bridge --rm dokku/wait -p 5432
-
-wget https://github.com/ufoscout/docker-compose-wait/releases/download/2.9.0/wait
-chmod +x wait
-./wait
 
 # Create databases.
 docker exec -ti -u postgres postgres_database psql
