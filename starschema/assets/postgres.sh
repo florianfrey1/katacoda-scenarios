2d6384faa09a1550685c9370b4becb7a3a1158be
Florian Frey <florianfrey@live.de>
2021-09-30 11:29:20 +0200

    changed wait script

--- starschema/assets/postgres.sh
+++ starschema/assets/postgres.sh
@@ -3,16 +3,14 @@ docker-compose up -d
 
 # Wait for the postgres container to start up using the small
 # dokku/wait container: https://github.com/dokku/docker-wait.
-docker run --net bridge --rm dokku/wait -p 5432
+# docker run --net bridge --rm dokku/wait -p 5432
+
+wget https://github.com/ufoscout/docker-compose-wait/releases/download/2.9.0/wait
+chmod +x wait
+/wait
 
 # Create databases.
-docker exec -ti -u postgres postgres_database psql -f ./databases/relational-schema.sql \q
-
-# Initialize the direktory for the node.js projekt (in the
-# current /root folder).
-npm init -y
-
-# Install the pg module via npm. The pg module is used to
-# connect from node js to the postgres databse:
-# https://github.com/brianc/node-postgres.
-npm i pg
+docker exec -ti -u postgres postgres_database psql
+CREATE DATABASE relational;
+CREATE DATABASE star;
+\q
