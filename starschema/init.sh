1d2c558b6c7b3ddd663875e798913fe547ae72f5
Florian Frey <florianfrey@live.de>
2021-09-30 15:03:51 +0200

    grouped

--- starschema/init.sh
+++ starschema/init.sh
@@ -19,11 +19,13 @@ echo "   Dateien ✅"
 echo "   Postgres-Datenbank ❌"
 echo "   NodeJS-Umgebung ❌"
 
-# Run the postgres database via the docker-compose command
-docker-compose up -d &> /dev/null
+{
+    # Run the postgres database via the docker-compose command
+    docker-compose up -d
 
-# Wait for the postgres container to boot up
-./wait-for-it.sh -t 0 127.0.0.1:5432 &> /dev/null
+    # Wait for the postgres container to boot up
+    ./wait-for-it.sh -t 0 127.0.0.1:5432
+} &> /dev/null
 
 clear
 echo "Umgebung wird vorbereitet..."
@@ -31,15 +33,19 @@ echo "   Dateien ✅"
 echo "   Postgres-Datenbank ✅"
 echo "   NodeJS-Umgebung ❌"
 
-# Initialize the direktory for the node.js projekt (in the
-# current /root folder).
-npm init -y &> /dev/null
+{
+    # Initialize the direktory for the node.js projekt (in the
+    # current /root folder).
+    npm init -y
 
 
-# Install the pg module via npm. The pg module is used to
-# connect from node js to the postgres databse:
-# https://github.com/brianc/node-postgres.
-npm i pg &> /dev/null
+    # Install the pg module via npm. The pg module is used to
+    # connect from node js to the postgres databse:
+    # https://github.com/brianc/node-postgres.
+    npm i pg
+} &> /dev/null
+
+stty -flusho
 
 clear
 echo "Umgebung ist bereit."
@@ -47,5 +53,4 @@ echo "   Dateien ✅"
 echo "   Postgres-Datenbank ✅"
 echo "   NodeJS-Umgebung ✅"
 
-stty -flusho
 stty echo
