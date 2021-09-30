220344ba6c84e5181c5b2a9866468f6be83afd65
Florian Frey <florianfrey@live.de>
2021-09-30 13:13:53 +0200

    new sqls

---
+++ starschema/assets/create-databases.sh
@@ -0,0 +1,2 @@
+psql -f star-database.create star_database
+psql -f star-database.create relational_database
--- starschema/assets/create-databases.sql
+++ starschema/assets/create-databases.sql
@@ -1,12 +1,2 @@
 CREATE DATABASE relational_database;
-CREATE TABLE IF NOT EXISTS product (
-    name VARCHAR PRIMARY KEY
-    description VARCHAR
-);
 CREATE DATABASE star_database;
-CREATE TABLE IF NOT EXISTS sales (
-    sales INTEGER,
-    revenue INTEGER,
-    quantity INTEGER,
-    discount INTEGER
-);
