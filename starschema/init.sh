ec42b6b8fdf1af0d64ddf2ebab38726b701a9581
Florian Frey <florianfrey@live.de>
2021-09-30 14:34:23 +0200

    change

--- starschema/init.sh
+++ starschema/init.sh
@@ -2,13 +2,15 @@
 
 stty flusho
 stty -echo
+clear
+echo "Die Umgebung wird vorbereitet..."
 
 ./setup.sh &> /dev/null
 
 stty -flusho
 
 clear
-echo "Die Umgebung ist jetzt bereit!"
+echo "Die Umgebung ist bereit!"
 echo " - Die Postgres-Datenbank ist bereit"
 echo " - Das NodeJS-Projekt ist angelegt"
 
