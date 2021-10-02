642cd48a9d13b07d7b8361dffebe86a3631e2947
Florian Frey <florianfrey@live.de>
2021-10-02 14:44:33 +0200

    wait

--- starschema/init.sh
+++ starschema/init.sh
@@ -4,13 +4,14 @@ function print_status {
     clear
     echo "Umgebung wird vorbereitet..."
     echo "   Dateien $1"
-    echo "   Postgres-Datenbank $2"
+    echo "   Postgres-Datenbank laden $2"
+    echo "   Postgres-Datenbank starten $3"
 }
 
 # stty flusho
 stty -echo
 
-print_status ⏳ ⏳ 
+print_status ⏳ ⏳ ⏳ 
 
 {
     until [ -f ./create-databases.sql ]
@@ -19,10 +20,11 @@ print_status ⏳ ⏳
     done
 } &> /dev/null
 
-print_status ✅ ⏳ 
+print_status ✅ ⏳ ⏳ 
 
 # Run the postgres database via the docker-compose command
 docker-compose up -d &> /dev/null
+print_status ✅ ✅ ⏳ 
 
 until docker exec root_postgres_1 psql -c "\c demo" &> /dev/null
 do
@@ -32,7 +34,7 @@ done
 # Wait for the postgres container to boot up
 # ./wait-for-it.sh -t 0 127.0.0.1:5432
 
-print_status ✅ ✅ 
+print_status ✅ ✅ ✅
 
 touch /root/environment.ready
 
