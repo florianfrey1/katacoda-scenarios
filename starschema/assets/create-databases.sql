6439adbf5d696fe01a026668c45e18410f22e114
Florian Frey <florianfrey@live.de>
2021-10-02 13:16:39 +0200

    change

--- starschema/assets/create-databases.sql
+++ starschema/assets/create-databases.sql
@@ -1,7 +1,6 @@
-CREATE DATABASE relational_database;
-CREATE DATABASE star_database;
+CREATE DATABASE demo;
 
-\c relational_database
+\c demo
 
 CREATE TABLE IF NOT EXISTS Kunde (
     id SERIAL PRIMARY KEY,
@@ -49,20 +48,6 @@ INSERT INTO Position (bestellung_id, artikel_id, menge) VALUES
     (2, 2, 4),
     (3, 1, 2);
 
-\c star_database
-
-CREATE TABLE IF NOT EXISTS Kunde (
-    id SERIAL PRIMARY KEY,
-    vorname VARCHAR NOT NULL,
-    nachname VARCHAR NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS Artikel (
-    id SERIAL PRIMARY KEY,
-    bezeichnung VARCHAR NOT NULL,
-    preis NUMERIC(5, 2) NOT NULL CHECK(preis >= 0)
-);
-
 CREATE TABLE IF NOT EXISTS Verkauf (
     id SERIAL PRIMARY KEY,
     kunde_id SERIAL REFERENCES Kunde(id),
@@ -71,16 +56,6 @@ CREATE TABLE IF NOT EXISTS Verkauf (
     umsatz NUMERIC(5, 2) NOT NULL CHECK(umsatz >= 0),
     menge INTEGER CHECK(menge > 0) DEFAULT 1
 );
-
-INSERT INTO Kunde (vorname, nachname) VALUES
-    ('Paul', 'Müller'),
-    ('Paula', 'Schmidt');
-
-INSERT INTO Artikel (bezeichnung, preis) VALUES
-    ('Schaumbad', 3.95),
-    ('Seife', 1.35),
-    ('Quitscheente', 5.49),
-    ('Quatsch', 0.01);
 
 -- INSERT INTO Verkauf (kunde_id, artikel_id, datum, umsatz, menge) VALUES
 --     (1, 1, '2020-12-10 08:33:12', 7.90, 2),
