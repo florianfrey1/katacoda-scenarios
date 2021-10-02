dc9c72044eaae2a582ee479e09fcb7eed37d5be9
Florian Frey <florianfrey@live.de>
2021-10-02 14:33:50 +0200

    progress

--- starschema/init.sh
+++ starschema/init.sh
@@ -1,46 +1,58 @@
 #!/bin/bash
 
-function print_status {
-    clear
+# function print_status {
+#     clear
 
-    if $1; then
-        echo "Umgebung ist bereit."
-    else
-        echo "Umgebung wird vorbereitet..."
-    fi
+#     if $1; then
+#         echo "Umgebung ist bereit."
+#     else
+#         echo "Umgebung wird vorbereitet..."
+#     fi
 
-    echo "   Dateien $2"
-    echo "   Postgres-Datenbank $3"
-}  
+#     echo "   Dateien $2"
+#     echo "   Postgres-Datenbank $3"
+# }
+
+function ProgressBar {
+    let _progress=(${1}*100/${2}*100)/100
+    let _done=(${_progress}*4)/10
+    let _left=40-$_done
+
+    _fill=$(printf "%${_done}s")
+    _empty=$(printf "%${_left}s")
+
+    printf "\rProgress : [${_fill// /#}${_empty// /-}]"
+}
 
 # stty flusho
 stty -echo
 
-print_status false ❌ ❌ 
+# print_status false ❌ ❌ 
 
 {
     until [ -f ./create-databases.sql ]
     do
         sleep 1
     done
+    ProgressBar 1 3
 } &> /dev/null
 
-print_status false ✅ ❌ 
+# print_status false ✅ ❌ 
 
-# {
-    # Run the postgres database via the docker-compose command
-    docker-compose up -d &> /dev/null
+# Run the postgres database via the docker-compose command
+docker-compose up -d &> /dev/null
+ProgressBar 2 3
 
-    until docker exec root_postgres_1 psql -c "\c demo" &> /dev/null
-    do
-        sleep 1
-    done
+until docker exec root_postgres_1 psql -c "\c demo" &> /dev/null
+do
+    sleep 1
+done
+ProgressBar 3 3
 
-    # Wait for the postgres container to boot up
-    # ./wait-for-it.sh -t 0 127.0.0.1:5432
-# } &> /dev/null
+# Wait for the postgres container to boot up
+# ./wait-for-it.sh -t 0 127.0.0.1:5432
 
-print_status true ✅ ✅ 
+# print_status true ✅ ✅ 
 
 touch /root/environment.ready
 
