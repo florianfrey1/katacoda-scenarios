af679a83397d1c8faa06d81da7deaa491cae1330
Florian Frey <florianfrey@live.de>
2021-10-02 13:54:40 +0200

    test

--- starschema/init.sh
+++ starschema/init.sh
@@ -31,35 +31,19 @@ print_status false ✅ ❌
     # Run the postgres database via the docker-compose command
     docker-compose up -d
 
-    # Wait for the postgres container to boot up
-    ./wait-for-it.sh -t 0 127.0.0.1:5432
+    until docker exec -it root_postgres_1 psql -c "\c demo" &> /dev/null
+    do
+        sleep 1
+    done
+
+    # # Wait for the postgres container to boot up
+    # ./wait-for-it.sh -t 0 127.0.0.1:5432
 } &> /dev/null
 
 print_status true ✅ ✅ 
 
+docker exec -it root_postgres_1 psql demo
 touch /root/environment.ready
 
 stty -flusho
 stty echo
-
-# until docker exec root_postgres_1 pg_isready ; do sleep 5 ; done
-# echo  "pg is ready"
-
-until docker exec -it root_postgres_1 psql -c "\c demo" &> /dev/null
-do
-    echo "waiting for postgres container..."
-    sleep 1
-done
-
-echo "READY"
-
-docker exec -it root_postgres_1 psql demo
-
-# docker ps
-
-# until ["$( docker container inspect -f '{{.State.Health}}' root_postgres_1 )" != "healthy"]
-# do
-#     echo "$( docker container inspect -f '{{.State.Health}}' root_postgres_1 )"
-#     sleep 1
-# done
-# docker exec -it root_postgres_1 psql demo
