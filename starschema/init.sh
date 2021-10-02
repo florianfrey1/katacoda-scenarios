fcc283528d9520e3a16e0f366f9965922c386cd8
Florian Frey <florianfrey@live.de>
2021-10-02 14:19:55 +0200

    start

--- starschema/init.sh
+++ starschema/init.sh
@@ -13,8 +13,8 @@ function print_status {
     echo "   Postgres-Datenbank $3"
 }  
 
-stty flusho
-stty -echo
+# stty flusho
+# stty -echo
 
 print_status false ❌ ❌ 
 
@@ -27,25 +27,25 @@ print_status false ❌ ❌
 
 print_status false ✅ ❌ 
 
-{
+# {
     # Run the postgres database via the docker-compose command
-    docker-compose up -d
+    docker-compose up
 
-    until docker exec -it root_postgres_1 psql -c "\c demo" &> /dev/null
-    do
-        sleep 1
-    done
+    # until docker exec -it root_postgres_1 psql -c "\c demo" &> /dev/null
+    # do
+    #     sleep 1
+    # done
 
     # # Wait for the postgres container to boot up
     # ./wait-for-it.sh -t 0 127.0.0.1:5432
-} &> /dev/null
+# } &> /dev/null
 
 print_status true ✅ ✅ 
 
-touch /root/environment.ready
+# touch /root/environment.ready
 
-stty -flusho
-stty echo
+# stty -flusho
+# stty echo
 
 docker exec -it root_postgres_1 psql demo
 \dt
