c94a44606b6839c2a8aced1d78c49b3acb83ef3c
Florian Frey <florianfrey@live.de>
2021-10-02 14:21:05 +0200

    wait

--- starschema/init.sh
+++ starschema/init.sh
@@ -29,7 +29,7 @@ print_status false ✅ ❌
 
 # {
     # Run the postgres database via the docker-compose command
-    docker-compose up
+    docker-compose up -d
 
     # until docker exec -it root_postgres_1 psql -c "\c demo" &> /dev/null
     # do
